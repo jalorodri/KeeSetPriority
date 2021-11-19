c0ef3c40f403329ae5c5a1e161c9aa60e141221a
jalorodri <jalonrodri@gmail.com>
2021-11-19 00:02:17 -0500

    Optimized compilation BAT file

--- CreatePLGX.bat
+++ CreatePLGX.bat
@@ -1,29 +1,15 @@
 cd %cd%
 cd ..
 
-rmdir KeeSetPriority\bin /s /q
-rmdir KeeSetPriority\obj /s /q
-rmdir KeeSetPriority\.vs /s /q
-
 mkdir .KeeSetPriority_temp
-mkdir .KeeSetPriority_temp\.git
-move KeeSetPriority\.gitattributes .KeeSetPriority_temp
-move KeeSetPriority\.gitignore .KeeSetPriority_temp
-move KeeSetPriority\.git\* .KeeSetPriority_temp\.git
-move KeeSetPriority\LICENSE.md .KeeSetPriority_temp
-move KeeSetPriority\README.md .KeeSetPriority_temp
-move KeeSetPriority\version.info .KeeSetPriority_temp
+mkdir .KeeSetPriority_temp\Properties
+copy KeeSetPriority\*.cs .KeeSetPriority_temp
+copy KeeSetPriority\Properties\* .KeeSetPriority_temp\Properties
+copy KeeSetPriority\KeeSetPriority.csproj .KeeSetPriority_temp
 
-"C:\Program Files\KeePass Password Safe 2\KeePass.exe" --plgx-create %cd%\KeeSetPriority
+"C:\Program Files\KeePass Password Safe 2\KeePass.exe" --plgx-create %cd%\.KeeSetPriority_temp
 
-move .KeeSetPriority_temp\.gitattributes KeeSetPriority
-move .KeeSetPriority_temp\.gitignore KeeSetPriority
-move .KeeSetPriority_temp\.git\* KeeSetPriority\.git
-move .KeeSetPriority_temp\LICENSE.md KeeSetPriority
-move .KeeSetPriority_temp\README.md KeeSetPriority
-move .KeeSetPriority_temp\version.info KeeSetPriority
 rmdir .KeeSetPriority_temp /s /q
-
-move KeeSetPriority.plgx KeeSetPriority
-
-REM pause
+move .KeeSetPriority_temp.plgx KeeSetPriority\KeeSetPriority.plgx
+pause
+exit
