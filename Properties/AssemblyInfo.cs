e2e470d4ba55aba0fbdaf6eedbcebede5478afdc
jalorodri <jalonrodri@gmail.com>
2021-11-21 20:44:09 -0500

    Updated to v0.8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5")]
+[assembly: AssemblyVersion("0.8")]
